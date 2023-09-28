bcb57d2eeee4bb4bf96b4e719792c35cb8e40e05
rakibulhasan933 <rakibulmd933@gmail.com>
2023-09-28 17:18:39 +0600

    [ADD] single route add id

---
+++ src/app/api/createNoteBook/[noteId]/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/db/prismadb";
+
+interface ParamsIProps {
+	params: UserIDProps
+};
+interface UserIDProps {
+	noteId: string
+};
+
+export async function GET(request: Request, { params }: ParamsIProps) {
+	try {
+		const { noteId } = params;
+		const note = await prisma?.note.findUnique({
+			where: {
+				id: noteId,
+			},
+		});
+
+		if (!note) {
+			return NextResponse.json({ message: "Note Not Found" }, { status: 404 });
+		};
+		console.log({ note });
+		return NextResponse.json({ note });
+
+	} catch (error) {
+		return NextResponse.json({ message: "GET Error", error }, { status: 500 })
+	};
+};
